6b92babdd1d0c7aa9b55f7d630108b42556f689c
Mikael Greenwood <mikaelanimates@gmail.com>
2024-11-05 13:20:07 -0800

    part 3 Health gauge for boss, hitbox changes,

---
+++ Assets/Scripts/Enemies/Bosses/BossHealthGauge.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealthGauge : MonoBehaviour {
+    [SerializeField] public GameObject _healthBar;
+    [SerializeField] public GameObject _healthBackground;
+
+    private void Update() {
+        if (gameObject.GetComponent<PlayerAwarenessController>().AwareOfPlayer) {
+            _healthBar.SetActive(true);
+            _healthBackground.SetActive(true);
+            _healthBar.GetComponent<Image>().fillAmount = gameObject.GetComponent<HealthController>().RemainingHealthPercentage;
+        }
+        if (gameObject.GetComponent<HealthController>().getHealth() <= 15) {
+            _healthBackground.SetActive(false);
+            _healthBar.SetActive(false);
+        }
+    }
+}
