1168338ad3fa423218023e694971cb5094684132
Mikael Greenwood <mikaelanimates@gmail.com>
2024-12-03 18:32:46 -0800

    Restart

--- Assets/Scripts/UI/Restart.cs
+++ Assets/Scripts/UI/Restart.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class Restart : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void Respawn(){
+        if (SceneManager.GetActiveScene().name == "Level 1" || SceneManager.GetActiveScene().name == "Level1Boss") SceneManager.LoadSceneAsync("Level 1");
+        if (SceneManager.GetActiveScene().name == "Level 2" || SceneManager.GetActiveScene().name == "Level2Boss") SceneManager.LoadSceneAsync("Level 2");
+        if (SceneManager.GetActiveScene().name == "Level 3" || SceneManager.GetActiveScene().name == "Level3Boss") SceneManager.LoadSceneAsync("Level 3");
     }
 }
