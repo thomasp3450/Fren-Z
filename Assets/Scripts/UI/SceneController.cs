17577887a7982cc74a59a0f8af33745b171b5654
thomasp3450 <food3450@gmail.com>
2024-12-04 12:51:32 -0800

    cutscene rendering changes

--- Assets/Scripts/UI/SceneController.cs
+++ Assets/Scripts/UI/SceneController.cs
@@ -37,13 +37,7 @@ public class SceneController : MonoBehaviour
     }
 
     public void NextLevel(){
-        if (SceneManager.GetActiveScene().name == "Level 1") {
-            player.GetComponent<PlayerController>().progressData.SetProgressData(1, player.GetComponent<PlayerController>()._amountOfBloodBombs, player.GetComponent<PlayerController>()._amountOfSyringes);
-        }
-        if (SceneManager.GetActiveScene().name == "Level 2") player.GetComponent<PlayerController>().progressData.SetProgressData(2, player.GetComponent<PlayerController>()._amountOfBloodBombs, player.GetComponent<PlayerController>()._amountOfSyringes);
-        if (SceneManager.GetActiveScene().name == "Level 3") player.GetComponent<PlayerController>().progressData.SetProgressData(3, player.GetComponent<PlayerController>()._amountOfBloodBombs, player.GetComponent<PlayerController>()._amountOfSyringes);
         SceneManager.LoadSceneAsync(scene.buildIndex + 1);
-        transitionAnimation.Play("FadeOut");
         scene = SceneManager.GetActiveScene();
     }
 
