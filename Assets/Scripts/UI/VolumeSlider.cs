9b48c4327ab571a6e8850879cc6a9b80430bb202
Mikael Greenwood <mikaelanimates@gmail.com>
2024-12-03 20:05:28 -0800

    Volume slider

---
+++ Assets/Scripts/UI/VolumeSlider.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+public class VolumeSlider : MonoBehaviour {
+    private float _volume;
+    [SerializeField] GameObject _audioManager;
+    [SerializeField] Slider _volumeSlider;
+    void Start(){
+        _audioManager = GameObject.Find("AudioManager");
+    }
+    void Update () {
+        if (_volumeSlider != null) _volume = _volumeSlider.value;
+        _audioManager.GetComponent<AudioManager>().musicSource.volume = _volume;
+        _audioManager.GetComponent<AudioManager>().SFXSource.volume = _volume;
+    }
+}
